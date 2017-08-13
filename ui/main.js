08da0686884e34cd333b0a50bb34071942acc638
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-13 10:36:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,11 @@ alert("hello js");
 
 
 var element1 = document.getElementById('dino');
-
+var left = 0;
 element1.onclick = function()
 {
  element1.style.marginLeft = '100px';
+ 
+ var left = left + 200;
 
 };
