07388f1fca82a9a595df1210f1a8e976c000ab42
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-13 22:54:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,19 +31,17 @@ click.onclick = function(){
 
 var name1 = document.getElementById('name');
 var name2 = name1.value;
-var list = '';
 var submit = document.getElementById('submit');
-submit.onclick = function() {
-    
-    var names =['name1','name2','name3','name4'];
-    for(var i=0; i<=names.length; i++)
-{
-    
-        list = '<li>'+ names[i] + '</li>';
-        ' 
+submit.onclick = function () {
+     var names = ['name1','name2','name3','name4'];
+     var list = '';
+    for (var i=0; i< names.length; i++){
+     
+        list += '<li>'+ names[i] + '</li>';
+        
 }
     
     var Namelist = document.getElementById('NameList');
     Namelist.innerHTML = list;
 };
-''
+
