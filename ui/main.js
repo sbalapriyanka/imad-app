f6991d4fd744ec30e552ae67634a94b1dfc7d71e
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-13 10:54:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,15 @@
 console.log('Loaded!');
 alert("hello js");
 
+
+
+
 var leftmargin = 0;
 var element = document.getElementById('dino');
 function move(){
     leftmargin = leftmargin +10;
-}  element.style.marginLeft = leftmargin + 'px';
+ element.style.marginLeft = leftmargin + 'px';
+}
 
 element.onclick = function(){
     var position = setInterval(move,300);
