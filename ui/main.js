e6c2fd77cbe4654059f5f92e66efb7d19a302e35
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-13 22:38:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 alert("hello js");
 
-
+///
 
 
 var leftmargin = 0;
@@ -16,6 +16,7 @@ element.onclick = function(){
     var position = setInterval(move,300);
 };
 
+///
 
 var counter = 0;
 var click = document.getElementById('click');
@@ -25,3 +26,25 @@ click.onclick = function(){
     counter = counter+1;
     times.innerHTML = counter.toString();
 }
+
+
+
+var name1 = document.getElementById('name');
+var name2 = name1.value;
+var list = '';
+var submit = document.getElementById('submit');
+submit.onclick = function() {
+    
+    
+    
+    var names = ['name1','name2','name3','name4];
+    for(var i = 0; i<=names.length; i++)
+    {
+        list = '<li>'+ names[i] + '<li>';
+        ' 
+    }
+    
+    var Namelist = document.getElementById('Namelist');
+    Namelist.innerHTML = list;
+};
+
