43fa8749c03935d5075be7c02b6bef00bf013293
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-16 11:50:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ app.get('/ThirdOne', function (req, res) {
   res.send(createone3(ThirdOne));
 });
 /////////////
-var pool = new pool (config);
+var pool = new Pool(config);
 app.get('/test-db',function (req, res){
     pool.query('SELECT * FROM test' , function(err, result){
         if(err)
