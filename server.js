e76c677ebf49441a016edc225ae5590b0c881173
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-16 20:55:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/', function (req, res) {
 
 app.get('/FirstOne', function (req, res) {
     
-    pool.query("SELECT * FROM article1 WHERE title = 'arwar'", [req.params.FirstOne], function(err,result)
+    pool.query('SELECT * FROM article1 WHERE title = arwar ', [req.params.FirstOne], function(err,result)
  {
      if (err)
      {
