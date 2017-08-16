4fbd9545ddda9148532f87392c6f4e882fa11052
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-16 20:00:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,6 +70,23 @@ app.get('/', function (req, res) {
 
 
 
+app.get('/FirstOne', function (req, res) {
+ pool.query('SELECT * FROM article1 ', function(err,result)
+ {
+     if (err)
+     {
+         res.status(500).send(err.toString());
+     }else{
+         if(result.rows.length === 0) {
+             res.status(404).send('Article Not found');
+         }else {
+             var articledata = result.rows[0];
+             res.send(createTemplate(articledata));
+             }
+     }
+ 
+});
+});
 
 var SecondOne = {
     title: 'My second article',
