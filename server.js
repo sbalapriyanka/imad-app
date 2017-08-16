32cb225a684e12652fe2ab041d5b9157435d2f4c
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-16 11:52:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -172,6 +172,21 @@ app.get('/ThirdOne', function (req, res) {
   res.send(createone3(ThirdOne));
 });
 /////////////
+
+var pool = new Pool(config);
+app.get('/test-db',function (req, res){
+    pool.query('SELECT * FROM test' , function(err, result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+            else
+         {   
+        res.send(JSON.stringify(result));
+    }
+});
+});
+///////////
 ///////////////
 var counter = 0;
 app.get('/counter', function(req,res) {
