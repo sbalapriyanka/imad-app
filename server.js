f7397265fa66f4c0b41c580a113a83bd51ec6f5a
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-26 20:51:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ app.get('/hash/:input',function(req,res){
 function hash(input,salt)
 {
     var hash = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return["pbkdf2","10000",salt,hash.toString('hex')].join($);
+    return["pbkdf2","10000",salt,hash.toString('hex')].join('$');
 }
 
 
