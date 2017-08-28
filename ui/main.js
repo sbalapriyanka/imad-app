c2a4c8740663e952862a9ccb97e0ce498c55fcf6
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-28 13:24:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,6 +79,6 @@ console.log(username);
 console.log(password);
 request.open('POST','http://balapriyankasomasekaran.imad.hasura-app.io/login',true);
 request.setRequestHeader('Content-Type','application/json');
-request.send(JSON.stringify({username: username, password: pass}));
+request.send(JSON.stringify({username: username, password: password}));
 };
 
