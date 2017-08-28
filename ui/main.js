c303082450a2566fa095d0eb9c455115468ded64
sbalapriyanka <30665301+sbalapriyanka@users.noreply.github.com>
2017-08-28 20:21:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,13 +7,13 @@ alert("hello js");
 var leftmargin = 0;
 var element = document.getElementById('dino');
 function move(){
-    leftmargin = leftmargin +10;
- element.style.marginLeft = leftmargin + 'px';
+leftmargin = leftmargin +10;
+element.style.marginLeft = leftmargin + 'px';
 }
 
 clearInterval(element);
 element.onclick = function(){
-    var position = setInterval(move,300);
+var position = setInterval(move,300);
 };
 
 ///
@@ -22,9 +22,9 @@ var counter = 0;
 var click = document.getElementById('click');
 
 click.onclick = function(){
-    var times = document.getElementById('times')
-    counter = counter+1;
-    times.innerHTML = counter.toString();
+var times = document.getElementById('times')
+counter = counter+1;
+times.innerHTML = counter.toString();
 }
 
 //////
@@ -33,16 +33,16 @@ var name1 = document.getElementById('name');
 var name2 = name1.value;
 var submit = document.getElementById('submit');
 submit.onclick = function () {
-     var names = ['name1','name2','name3','name4'];
-     var list = '';
-    for (var i=0; i< names.length; i++){
-     
-        list += '<li>'+ names[i] + '</li>';
-        
-}
+ var names = ['name1','name2','name3','name4'];
+ var list = '';
+for (var i=0; i< names.length; i++){
+ 
+    list += '<li>'+ names[i] + '</li>';
     
-    var Namelist = document.getElementById('NameList');
-    Namelist.innerHTML = list;
+}
+
+var Namelist = document.getElementById('NameList');
+Namelist.innerHTML = list;
 };
 
 ////////////////////////////////////////////////////////////////////
@@ -51,30 +51,30 @@ submit.onclick = function () {
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    
 
-    //create a request object
-    var request = new XMLHttpRequest(); 
-    
-    //capture the response and store in variable 
-    
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //TAKE AACTION
-            if(request.status === 200){
-            alert('login success');
-        }
-            else if(request.status===403){
-            alert('login/password is incorrect');
-            }
-            else if(request.status === 500)
-            {
-                alert('internal server error');
-            }
-        }
-    };
 
-    //extract user name and password
+//create a request object
+var request = new XMLHttpRequest(); 
+
+//capture the response and store in variable 
+
+request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        //TAKE AACTION
+        if(request.status === 200){
+        alert('login success');
+    }
+        else if(request.status===403){
+        alert('login/password is incorrect');
+        }
+        else if(request.status === 500)
+        {
+            alert('internal server error');
+        }
+    }
+};
+
+//extract user name and password
 var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 console.log(username);
